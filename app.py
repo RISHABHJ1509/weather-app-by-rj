b58f7b278d239f629b5ad8a8122b3a256450492f
RISHABHJ1509 <jaiswalrishabh1509@gmail.com>
2023-10-27 21:24:23 +0530

    code changed for city

--- app.py
+++ app.py
@@ -13,12 +13,14 @@ def get_weatherdata():
 
     param = {
         'q':request.form.get("city"),
-        'appid':request.form.get('appid'),
+        # 'appid':request.form.get('appid')
+        'appid':'411483bfc5bc843550fec681b795adc9',
         'units':request.form.get('inits')
         }
     response = requests.get(url,params=param)
+    city = data['name']
     data = response.json()
-    return f"data : {data}"
+    return f"data : {data}, city : {city}"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
